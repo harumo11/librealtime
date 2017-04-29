40e380b583336b285d4ad8ec9cf5cbb9f23fa112
harumo11 <s_harumo@hotmail.co.jp>
2017-04-29 11:35:16 +0900

    [Add] Added new testPolicyThread.cpp

---
+++ example/testPolicyThread.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unistd.h>
+#include "../include/PolicyThread.hpp"
+
+using std::cout;
+using std::endl;
+
+void test(){
+	cout << "hello world" << endl;
+	sleep(1);
+}
+
+int main(int argc, char const* argv[])
+{
+
+	PolicyThread pth(test);
+	pth.start(20);
+	sleep(4);
+	cout << pth.getPriority() << endl;
+	pth.end();
+	
+	return 0;
+}
