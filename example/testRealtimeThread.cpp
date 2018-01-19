2db9730e9fc84ff1299c43a2dd854759537b12f3
Harumo <s_harumo@hotmail.co.jp>
2018-01-19 17:31:14 +0900

    [update] revise testRealtimeThread.cpp

--- example/testRealtimeThread.cpp
+++ example/testRealtimeThread.cpp
@@ -8,32 +8,29 @@
 
 using namespace std;
 
-std::vector<std::chrono::high_resolution_clock::time_point> vec;
+std::vector<std::chrono::high_resolution_clock::time_point> vec(2000);
+int i = 0;
 
 void test()
 {
-	std::chrono::high_resolution_clock::time_point point;
-	point = std::chrono::high_resolution_clock::now();
-	//std::cout << "Execute" << std::endl;
-	//std::cout << point.time_since_epoch().count() << std::endl;
-	vec.push_back(point);
+	auto point = std::chrono::high_resolution_clock::now();
+	vec[i] = point;
+	i++;
 }
 
 int main(int argc, char const* argv[])
 {
 	RealtimeThread th(1000, test);
 	th.start(true);
-	sleep(10);
+	sleep(1);
 	th.end();
 
 	cout << "Recode time" << endl;
 	std::ofstream file("log.csv");
 	
-	int i = 0;
 	for(auto &e : vec){
 		file << e.time_since_epoch().count() << std::endl;
 		cout  << e.time_since_epoch().count() << std::endl;
-		i++;
 	}
 	cout << "[count] : " << i << std::endl;
 	file.close();
