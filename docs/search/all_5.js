1e370c5c691b3f1ab24e92c8c850fc0c016fe363
harumo11 <s_harumo@hotmail.co.jp>
2018-03-27 22:31:02 +0900

    [add] added docs

---
+++ docs/search/all_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['policythread',['PolicyThread',['../class_policy_thread.html',1,'PolicyThread'],['../class_policy_thread.html#a21c88f3ed2c7d0738fc8ceccfbe32a1b',1,'PolicyThread::PolicyThread()']]]
+];
