85576675af11e429078029d7fd36e82ef9059719
harumo11 <s_harumo@hotmail.co.jp>
2019-04-30 20:46:17 +0900

    [update] added comments in order to explain usage of those thead class.

--- example/tutorial_realtime_thread.cpp
+++ example/tutorial_realtime_thread.cpp
@@ -4,6 +4,10 @@
 #include <unistd.h>
 #include "../librealtime/RealtimeThread.hpp"
 
+// This program show that it is possible to use same as std::thread.
+// Particularly, please focus on passing argument.
+// You can pass arguments to Realtime and Policy thread class like a std::thread class.
+
 using namespace std;
 
 void test(std::vector<std::chrono::high_resolution_clock::time_point>& time_points)
@@ -14,6 +18,8 @@ void test(std::vector<std::chrono::high_resolution_clock::time_point>& time_poin
 int main(void)
 {	
 	std::vector<std::chrono::high_resolution_clock::time_point> time_record;
+	
+	// You can pass arguments to Realtime and Policy thread class like a std::thread class.
 	RealtimeThread th(100, test, time_record);
 	th.start(true);
 	sleep(10);
